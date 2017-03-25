9d30cb8c8a611dd71fd5e9ccc7ded90eb84ac10b
John <18702516203@126.com>
2017-03-25 14:45:11 +0800

    修复bug

--- static/js/topic.js
+++ static/js/topic.js
@@ -44,7 +44,7 @@ var Vue_App = new Vue({
         return item;
       });
       editor.create();
-
+      console.log(25588)
       this.getList(1, 15, "", "");
       document.querySelector("#searchKey").focus();
     }
